bce8a588af7f9950a9c4f081550c274837702dea
verraca <veronica.racca@gmail.com>
2019-06-05 19:36:56 -0300

    Add project files.

---
+++ miChango/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(miChango.Startup))]
+namespace miChango
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
