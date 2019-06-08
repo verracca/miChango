ef725f4f2691c8f66686690956430d7484fa032f
verraca <veronica.racca@gmail.com>
2019-06-08 17:45:26 -0300

    Add Models

---
+++ miChango/Models/ListaModels.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace miChango.Models
+{
+    public class Lista
+    {
+        [Key]
+        public int Id { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+
+    public class Product
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
